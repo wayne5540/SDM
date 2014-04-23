9ec1b0f5a53ee7d54e39c2a81b90629f5ebb1c9b
wayne-mac <w5540@yahoo.com.tw>
2014-04-23 23:32:18 +0800

    facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Starbucks::Application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  root "drinks#index"
+  resources :drinks
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
