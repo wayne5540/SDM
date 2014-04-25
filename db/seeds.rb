95b3cafcde661af002081df8063544a80e6d1f0a
wayne-mac <w5540@yahoo.com.tw>
2014-04-25 16:32:01 +0800

    add base materials seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Mtype.create(name: 'base')
-Mtype.create(name: 'syrup')
-Mtype.create(name: 'additive')
+
+#setup materials_type -> Mtype
+mtypes = ['base', 'syrup', 'additive']
+mtypes.each do |m|  Mtype.create(name: m) end
+
+#setup materials -> Matrials
+base_material = ['濃縮咖啡', '本日精選',
+ '玫瑰紅茶', '東方美人', '紅玉紅茶', '碧螺春',
+ '阿里山烏龍', '咖啡底液', '牛奶底液', '水果底液',
+ '牛奶', '豆漿']
+
+base_type = Mtype.find(1)
+base_material.each do |base| base_type.materials.create(name: base) end
+
+
 
 
 
