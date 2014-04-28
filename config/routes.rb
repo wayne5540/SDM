6526870856c46d156a3262167e07c1e0ca157d70
wayne-mac <w5540@yahoo.com.tw>
2014-04-28 14:20:07 +0800

    add static page controller 2 , home and contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Starbucks::Application.routes.draw do
+  root "static_pages#home"
+  get "static_pages/contact"
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  root "drinks#index"
   resources :drinks
   resources :materials
 
